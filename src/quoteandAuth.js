5f55219b9b94854997f8614931c538d556507fd7
notdaan <Danieljohnmc1@gmail.com>
2023-06-12 17:02:37 +1000

    updated to pass free code camp tests

--- src/quoteandAuth.js
+++ src/quoteandAuth.js
@@ -6,19 +6,20 @@ export default function QuoteAndAuth(props) {
     const {quote, getQuote} = props;
     return (
         <div className="App">
-        <div id="quote-box" style={{backgroundColor: quote.bgcolor}}>
-        <div className="quote-container">
+        <div id="quote" style={{backgroundColor: quote.bgcolor}}>
+        <div className="quote-container" id="quote-box">
         <h1 id="text" style={{color: quote.bgcolor}}><i className="fa fa-quote-left"></i> {quote.quote} <i className="fa fa-quote-right"> </i></h1>
         <h3 id="author" style={{color: quote.bgcolor}}>{quote.author}</h3>
           <div className="buttons">
-            <button 
+            <a 
             style={{color: quote.bgcolor}}
             type="submit"
             id="tweet-quote"
+            href="twitter.com/intent/tweet"
             onClick={() => {
               window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent('"'+ quote.quote + '" '  + quote.author))
             }}>
-            <i className="fa fa-twitter"></i></button>
+            <i className="fa fa-twitter"></i></a>
             <button
                 style={{color: quote.bgcolor}}
                 type="submit" 
